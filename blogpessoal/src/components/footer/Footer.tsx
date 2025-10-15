9d28cb2936cb0daafdd931ece01f5c5023b616c4
Henrique de Oliveira <henrique2801oliveira@gmail.com>
2025-10-15 09:35:00 -0300

    tag Link

--- blogpessoal/src/components/footer/Footer.tsx
+++ blogpessoal/src/components/footer/Footer.tsx
@@ -11,9 +11,15 @@ function Footer(){
                 </p>
                 <p className="text-lg">Acesse as nossas redes sociais</p>
                 <div className="flex gap-2">
-                    <LinkedinLogoIcon size={48} weight="bold"/>
-                    <FacebookLogoIcon size={48} weight="bold"/>
-                    <InstagramLogoIcon size={48} weight="bold"/>
+                    <a href="https://www.linkedin.com/in/henrique-de-oliveira-gomes" target="_blank">
+                        <LinkedinLogoIcon size={48} weight="bold"/>
+                    </a>
+                    <a href="https://www.facebook.com/henrique.d.gomes.58" target="_blank">
+                        <FacebookLogoIcon size={48} weight="bold"/>
+                    </a>
+                    <a href="https://www.instagram.com/henriquehsk" target="_blank">
+                        <InstagramLogoIcon size={48} weight="bold"/>
+                    </a>
                 </div>
             </div>
         </div>
